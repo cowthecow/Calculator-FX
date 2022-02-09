3eac9f59a9c291fc71e764680e8910cdacf66f4a
Cow the Cow <laserinfinite0@gmail.com>
2022-02-08 19:08:50 -0500

    Major updates to control and mechanics

--- Controller.java
+++ Controller.java
@@ -2,75 +2,201 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+
+    private String mainText = "0";
 
     @FXML
     TextField textField;
 
     @FXML
-    Button button1;
-    @FXML
-    Button button2;
-    @FXML
-    Button button3;
-    @FXML
-    Button button4;
-    @FXML
-    Button button5;
-    @FXML
-    Button button6;
-    @FXML
-    Button button7;
-    @FXML
-    Button button8;
-    @FXML
-    Button button9;
-    @FXML
-    Button button0;
+    Button someScientificNode;
 
     @FXML
-    Button remove;
-    @FXML
-    Button enter;
+    Button someStandardNode;
 
     @FXML
-    private void onButtonClicked(ActionEvent e) {
-        if (e.getSource().equals(button0)) {
-            textField.appendText("0");
+    Button someAboutUsNode;
+
+    @FXML
+    private void turnOnBasic(ActionEvent e) throws IOException {
+        Stage stage;
+
+        try {
+            stage = (Stage) (someScientificNode).getScene().getWindow();
+        }catch (RuntimeException ignore) {
+            stage = (Stage) (someAboutUsNode).getScene().getWindow();
         }
-        if (e.getSource().equals(button1)) {
-            textField.appendText("1");
+
+        Parent root = FXMLLoader.load(getClass().getResource("simple.fxml"));
+        stage.setTitle("QuickMaths V1.3 SIMPLE");
+        stage.setScene(new Scene(root, 420, 585));
+    }
+
+    @FXML
+    private void turnOnScientific(ActionEvent e) throws IOException {
+        Stage stage;
+
+        try {
+            stage = (Stage) (someStandardNode).getScene().getWindow();
+        }catch (RuntimeException ignore) {
+            stage = (Stage) (someAboutUsNode).getScene().getWindow();
         }
-        if (e.getSource().equals(button2)) {
-            textField.appendText("2");
+
+        Parent root = FXMLLoader.load(getClass().getResource("scientific.fxml"));
+        stage.setTitle("QuickMaths V1.3 SCIENTIFIC");
+        stage.setScene(new Scene(root, 420, 585));
+    }
+
+    @FXML
+    private void turnOnAboutUs(ActionEvent e) throws IOException {
+        Stage stage;
+
+        try {
+            stage = (Stage) (someStandardNode).getScene().getWindow();
+        }catch (RuntimeException ignore) {
+            stage = (Stage) (someScientificNode).getScene().getWindow();
         }
-        if (e.getSource().equals(button3)) {
-            textField.appendText("3");
+
+        Parent root = FXMLLoader.load(getClass().getResource("aboutUs.fxml"));
+        stage.setTitle("QuickMaths V1.3");
+        stage.setScene(new Scene(root, 420, 585));
+    }
+
+    @FXML
+    private void onKeyPressed(KeyEvent e) {
+        if (e.getCode().toString().contains("DIGIT")) {
+            if (mainText.equals("0")) mainText = "";
+            mainText += e.getCode().toString().substring(5, 6);
         }
-        if (e.getSource().equals(button4)) {
-            textField.appendText("4");
+        if (e.getCode().toString().equals("BACK_SPACE")) {
+            if (mainText.length() == 1) {
+                mainText = "0";
+            } else {
+                mainText = mainText.substring(0, mainText.length() - 1);
+            }
+        } else {
+            System.out.println(e.getCode().toString());
         }
-        if (e.getSource().equals(button5)) {
-            textField.appendText("5");
+        textField.setText(mainText);
+    }
+
+    @FXML
+    private void onNumberClicked(ActionEvent e) {
+        if((mainText.charAt(mainText.length()-1)) == ')') return;
+
+        if (mainText.equals("0")) mainText = "";
+        mainText += e.getSource().toString().charAt(e.getSource().toString().length() - 2);
+        textField.setText(mainText);
+    }
+
+    @FXML
+    private void onOperatorClicked(ActionEvent e) {
+        if (mainText.length() == 0) return;
+        //Anything that counts as a number, including superscript
+        if (Character.isDigit(mainText.charAt(mainText.length() - 1)) || mainText.charAt(mainText.length() - 1) == '²' || mainText.charAt(mainText.length() - 1) == ')') {
+            mainText += e.getSource().toString().charAt(e.getSource().toString().length() - 2);
+            textField.setText(mainText);
         }
-        if (e.getSource().equals(button6)) {
-            textField.appendText("6");
+    }
+
+    //Advanced operators include squares, square roots, and inverse function
+    @FXML
+    private void onAdvancedOperatorClicked(ActionEvent e) {
+        String input = e.getSource().toString();
+
+        if (mainText.length() == 0) return;
+        if (!Character.isDigit(mainText.charAt(mainText.length() - 1))) return;
+
+        if (input.contains("power")) {
+            mainText += "²";
+        } else if (input.contains("root") || input.contains("inverse")) {
+            int lastOperator = 0;
+            for (int i = 0; i < mainText.length(); i++) {
+                if (isOperator(mainText.charAt(i))) lastOperator = i;
+            }
+            StringBuilder inserter = new StringBuilder(mainText);
+
+            if (input.contains("root")) {
+                inserter.insert(lastOperator == 0 ? 0 : lastOperator + 1, '√');
+            } else {
+                inserter.insert(lastOperator == 0 ? 0 : lastOperator + 1, "1/");
+            }
+            mainText = inserter.toString();
+        } else if (input.contains("negate")) {
+            int lastOperator = 0;
+            for (int i = 0; i < mainText.length(); i++) {
+                if (isOperator(mainText.charAt(i))) lastOperator = i;
+            }
+
+            int insertLocation = lastOperator == 0 ? 0 : lastOperator + 1;
+            StringBuilder inserter = new StringBuilder(mainText);
+            inserter.insert(insertLocation, "-");
+            mainText = inserter.toString();
+
         }
-        if (e.getSource().equals(button7)) {
-            textField.appendText("7");
+
+        textField.setText(mainText);
+    }
+
+    @FXML
+    private void onBackspaceClicked(ActionEvent e) {
+        if (e.getSource().toString().contains("clear")) {
+            mainText = "0";
+        } else if (e.getSource().toString().contains("del")) {
+            if (mainText.length() == 1) {
+                mainText = "0";
+            } else if (mainText.length() > 1) {
+                mainText = mainText.substring(0, mainText.length() - 1);
+            }
         }
-        if (e.getSource().equals(button8)) {
-            textField.appendText("8");
+        textField.setText(mainText);
+    }
+
+    @FXML
+    private void onBracketClicked(ActionEvent e) {
+        if (e.getSource().toString().contains("(")) {
+            if (mainText.equals("0")) {
+                mainText = "(";
+            } else {
+                if (isOperator(mainText.charAt(mainText.length() - 1))) {
+                    mainText += "(";
+                }
+            }
         }
-        if (e.getSource().equals(button9)) {
-            textField.appendText("9");
+        if (e.getSource().toString().contains(")")) {
+            if(!validRightBracket(mainText)) return;
+            if (mainText.equals("0")) {
+                mainText = "(";
+            } else {
+                if (!isOperator(mainText.charAt(mainText.length() - 1))) {
+                    mainText += ")";
+                }
+            }
         }
-        if (e.getSource().equals(remove)) {
-            if(textField.getText().length()>0)
-                textField.setText(textField.getText().substring(0, textField.getText().length()-1));
+        textField.setText(mainText);
+    }
+
+    private boolean validRightBracket(String s) {
+        int lbrackets = 0, rbrackets = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') lbrackets++;
+            if (s.charAt(i) == ')') rbrackets++;
         }
+        return (lbrackets > rbrackets);
+    }
+
+    private boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(';
     }
 }
--- Mechanics.java
+++ Mechanics.java
@@ -1,9 +1,95 @@
 package sample;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Mechanics {
 
+    //2+3*5 > 2 3 5 * +
+    //2*3*5 > 2 3 5 * *
+    //2*3/5 > 2 3 * 5 /
+    //2*3+5 > 2 3 * 5 +
+    //(2+3)*5 > 2 3 + 5 *
+    //2*(3+5) > 2 3 5 + *
 
+    //Algorithm
+    //If you see a number append it to postfix
+    //If you see a left bracket push it to stack
+    //If you see right bracket pop everything on the stack until you see the left bracket which is discarded
+    //If you see an operator push it to stack
 
+    //If your operator has higher or equal precedence then the one on top of the stack, push it into the stack
+    //If your operator has lower precedence then the one on top of the stack, pop everything on the stack until you get something with lower precedence than the current operator
 
+    public static boolean isSingleOperation(char c) {
+        return c == '√' || c == '²';
+    }
+
+    public static boolean isDoubleOperation(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    public static String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> operators = new Stack<>();
+        char savedOperator = ' ';
+
+        for(int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+
+            if(Character.isDigit(c) || c == '.') {
+                postfix.append(c);
+            }else if(isSingleOperation(c)) {
+                if(c == '√') {
+                    savedOperator = c;
+                }else {
+                    postfix.append(c);
+                }
+            }else if(isDoubleOperation(c)) {
+                postfix.append(savedOperator);
+                postfix.append(" ");
+                if(operators.size() == 0) {
+                    operators.push(c);
+                }else {
+                    if(getOperatorPrecedence(operators.peek()) > getOperatorPrecedence(c)) {
+                        while(getOperatorPrecedence(operators.peek()) > getOperatorPrecedence(c)) {
+                            postfix.append(operators.pop());
+                        }
+                    }else {
+                        operators.push(c);
+                    }
+                }
+            }else if(c == '(') {
+                operators.push(c);
+            }else if(c == ')') {
+                while(operators.peek() != '(') {
+                    postfix.append(operators.pop());
+                }
+                operators.pop();
+            }
+        }
+        while(operators.size()>0) {
+            postfix.append(operators.pop());
+        }
+        return postfix.toString();
+    }
+
+    public static int getOperatorPrecedence(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String infix = scanner.nextLine();
+        System.out.println(infixToPostfix(infix));
+    }
 
 }
